83bd070c41c2d9327d72cdedc6588710e6be18da
Wicaksana Pratama <wicaksanapratama@gmail.com>
2022-08-18 16:53:23 +0700

    add switcher component

--- src/libs/atoms/inputs/index.ts
+++ src/libs/atoms/inputs/index.ts
@@ -3,13 +3,16 @@ import { defineCustomElement } from 'vue';
 import Input from './input.ce.vue';
 import Dropdown from './dropdown.ce.vue';
 import InputGroup from './inputGroup.ce.vue';
+import Switcher from './switcher.ce.vue';
 
 const input = defineCustomElement(Input);
+const switcher = defineCustomElement(Switcher);
 const dropdown = defineCustomElement(Dropdown);
 const inputGroup = defineCustomElement(InputGroup);
 
 export function register() {
   customElements.define('ma-input', input);
+  customElements.define('ma-switcher', switcher);
   customElements.define('ma-dropdown', dropdown);
   customElements.define('ma-input-group', inputGroup);
 }
---
+++ src/libs/atoms/inputs/switcher.ce.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="ma-switcher">
+    <span><slot /></span>
+    <label class="switch">
+      <input type="checkbox" />
+      <span class="slider"></span>
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props: any = defineProps([]);
+</script>
+
+<style lang="scss">
+@use '../../../styles/global';
+@use '../../../styles/functions';
+@use '../../../styles/colors';
+
+.ma-switcher {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  gap: functions.toRem(10);
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  color: #424242;
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: functions.toRem(40);
+  height: functions.toRem(20);
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  border-radius: functions.toRem(32);
+}
+
+.slider:before {
+  position: absolute;
+  content: '';
+  height: 16px;
+  width: 16px;
+  left: 2px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  border-radius: 50%;
+}
+
+input:checked + .slider {
+  background-color: colors.$primary-1;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px colors.$primary-1;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(20px);
+  -ms-transform: translateX(20px);
+  transform: translateX(20px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
